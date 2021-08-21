820e7d0cf1adb7f417af3a903e04237f839f6eb5
Ridho894 <77140390+Ridho894@users.noreply.github.com>
2021-08-21 18:54:35 +0700

    push dikit habis solat lanjut lgi hehe

--- src/screens/Quiz.tsx
+++ src/screens/Quiz.tsx
@@ -1,10 +1,15 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { View, Text } from "react-native";
-import { Difficulty, getQuizQuestions } from "../utils/utils";
-
+import {
+  Difficulty,
+  getQuizQuestions,
+  Question,
+  QuestionState,
+} from "../utils/utils";
+import { Icon } from "react-native-elements";
 function Quiz() {
   const [loading, setLoading] = useState(false);
-  const [questions, setQuestions] = useState([]);
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
   const [userAnswers, setUserAnswers] = useState([]);
   const [score, setScore] = useState(0);
   const [gameOver, setGameOver] = useState(true);
@@ -16,11 +21,68 @@ function Quiz() {
     setGameOver(false);
 
     const newQuestions = getQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY);
+    console.log(newQuestions);
   };
 
+  useEffect(() => {
+    startQuiz();
+  }, []);
+
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>QUIZ</Text>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "flex-end",
+        position: "relative",
+        padding: 20,
+        backgroundColor: "#fff",
+      }}
+    >
+      <View style={{ flex: 1 }}>
+        <View
+          style={{
+            flexDirection: "row",
+            marginTop: 0,
+            justifyContent: "space-between",
+          }}
+        >
+          <Text style={{ fontSize: 16 }}>Questions</Text>
+          <Text
+            style={{
+              fontSize: 16,
+              color: "#006996",
+            }}
+          >
+            1/10
+          </Text>
+        </View>
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            marginTop: 33,
+            paddingRight: 10,
+          }}
+        >
+          <Text style={{ color: "#006996", fontSize: 16 }}>1</Text>
+        </View>
+      </View>
+      <View
+        style={{
+          padding: 20,
+          backgroundColor: "#006996",
+          alignItems: "center",
+          justifyContent: "center",
+          borderRadius: 300,
+          width: 50,
+          height: 50,
+          position: "absolute",
+          bottom: 20,
+          right: 20,
+        }}
+      >
+        <Icon type="entypo" name="plus" color="white" size={10} />
+      </View>
     </View>
   );
 }
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -6,8 +6,22 @@ export enum Difficulty{
     HARD = "hard"
 }
 
+export type Question = {
+    category: string;
+    correct_answer: string;
+    difficult: string;
+    incorrect_answers: string[];
+    question: string;
+    type: string;
+}
+
+export type QuestionState = Question & {answer: string[]}
+
 export const getQuizQuestions = async (amount: number, difficulty:Difficulty) => {
     const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}`
     const data = await (await fetch(endpoint)).json()
-    console.log(data)
+    return data.results.map((question: Question) => ({
+        ...question,
+        answers: _([...question.incorrect_answers, question.correct_answer])
+    }))
 }

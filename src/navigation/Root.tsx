e49c368c436d070708138564b9c084768360bf9f
Ridho894 <77140390+Ridho894@users.noreply.github.com>
2021-08-23 19:30:13 +0700

    menambah beberapa screen dan untuk quiz kurang menghitung nilai akhir

--- src/navigation/Root.tsx
+++ src/navigation/Root.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { Button, Icon } from "react-native-elements";
+import { Button } from "react-native-elements";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import Quiz from "../screens/Quiz";
 import Home from "../screens/Home";
 import colors from "../utils/colors";
@@ -13,36 +14,59 @@ const Tab = createBottomTabNavigator();
 const Root = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="HOME"
+      <Tab.Navigator>
+        <Tab.Screen
+          name={"HOME"}
           component={Home}
           options={{
             headerTitleAlign: "center",
             headerTintColor: "white",
             headerStyle: { backgroundColor: colors.blue },
+            tabBarActiveTintColor: colors.blue,
+            tabBarIcon: () => (
+              <MaterialCommunityIcons
+                name="home"
+                size={30}
+                color={colors.blue}
+              />
+            ),
           }}
         />
-        <Stack.Screen
-          name="QUIZ"
+        <Tab.Screen
+          name={"QUIZ"}
           component={Quiz}
           options={{
             headerTitleAlign: "center",
             headerTintColor: "white",
             headerStyle: { backgroundColor: colors.blue },
+            tabBarActiveTintColor: colors.blue,
+            tabBarIcon: () => (
+              <MaterialCommunityIcons
+                name="book"
+                size={30}
+                color={colors.blue}
+              />
+            ),
           }}
         />
-      </Stack.Navigator>
+      </Tab.Navigator>
     </NavigationContainer>
   );
 };
 
-const Dashboard = (props: any) => {
-  return (
-    <Tab.Navigator>
-      <Tab.Screen name={'Home'}/>
-    </Tab.Navigator>
-  );
-};
+// function Dashboard(props: any) {
+//   return <NavigationContainer></NavigationContainer>;
+// }
 
 export default Root;
+function props(
+  arg0: JSX.Element
+):
+  | ((props: {
+      focused: boolean;
+      color: string;
+      size: number;
+    }) => React.ReactNode)
+  | undefined {
+  throw new Error("Function not implemented.");
+}

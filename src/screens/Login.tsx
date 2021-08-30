308c3ca7f29f34524895252fada7cb4f3ac9edd3
Ridho894 <77140390+Ridho894@users.noreply.github.com>
2021-08-30 22:03:05 +0700

    add password login

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,44 +1,64 @@
-import React from "react";
-import { View, Text, Image, Dimensions } from "react-native";
+import React, { useState } from "react";
+import { View, Text, ScrollView, Image, Dimensions } from "react-native";
 import { TextInput } from "react-native-paper";
 import colors from "../utils/colors";
 import { Button } from "react-native-elements";
-
+import { Ionicons } from "@expo/vector-icons";
 const { height, width } = Dimensions.get("window");
 
 const Login = ({ navigation }: any) => {
+  const [showPassword, setShowPassword] = useState(false);
   return (
-    <View style={{ backgroundColor: "#f0f0f0", height: height }}>
-      <Image
-        source={require("../assets/Logo.png")}
-        style={{
-          height: height / 3,
-          width: width / 1.5,
-          alignSelf: "center",
-          marginTop: 30,
-        }}
-      />
-      <View style={{ marginTop: 30 }}>
-        <TextInput
-          dense={true}
-          label="Username"
-          placeholder="Masukkan Username"
-          mode="outlined"
-          theme={{ colors: { primary: colors.blue } }}
-          style={{ width: width / 1.1, alignSelf: "center" }}
-        />
-        <Button
-          title="GET STARTED"
-          buttonStyle={{
-            backgroundColor: colors.darkBlue,
-            width: width / 2,
+    <ScrollView>
+      <View style={{ backgroundColor: "#f0f0f0", height: height }}>
+        <Image
+          source={require("../assets/Logo.png")}
+          style={{
+            height: height / 3,
+            width: width / 1.5,
             alignSelf: "center",
             marginTop: 30,
           }}
-          onPress={() => navigation.navigate("BarNavigation")}
         />
+        <View style={{ marginTop: 30 }}>
+          <TextInput
+            dense={true}
+            label="Email"
+            placeholder="Masukkan Email"
+            mode="outlined"
+            theme={{ colors: { primary: colors.blue } }}
+            style={{ width: width / 1.1, alignSelf: "center" }}
+          />
+          <TextInput
+            dense={true}
+            label="Password"
+            secureTextEntry={!showPassword}
+            placeholder="Masukkan Password"
+            mode="outlined"
+            right={
+              <Ionicons
+                name={showPassword ? "eye-off-sharp" : "eye"}
+                size={24}
+                color="blue"
+                onPress={() => setShowPassword((prev) => !prev)}
+              />
+            }
+            theme={{ colors: { primary: colors.blue } }}
+            style={{ width: width / 1.1, alignSelf: "center", marginTop: 30 }}
+          />
+          <Button
+            title="LOGIN"
+            buttonStyle={{
+              backgroundColor: colors.darkBlue,
+              width: width / 1.1,
+              alignSelf: "center",
+              marginTop: 30,
+            }}
+            onPress={() => navigation.navigate("BarNavigation")}
+          />
+        </View>
       </View>
-    </View>
+    </ScrollView>
   );
 };
 

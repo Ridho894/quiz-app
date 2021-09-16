85af524fc27fc26af9a4edeb3a65f3ebb6b19fb0
Ridho894 <77140390+Ridho894@users.noreply.github.com>
2021-09-16 11:14:55 +0700

    confusing

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,14 +1,76 @@
-import React from "react";
-import { View, Text } from "react-native";
-import { Button } from "react-native-elements";
-
-const Home = (navigation: any) => {
+import React, { useState } from "react";
+import {
+  SafeAreaView,
+  View,
+  FlatList,
+  StyleSheet,
+  Text,
+  StatusBar,
+} from "react-native";
+const DATA = [
+  {
+    id: "1",
+    title: "Take coffee",
+  },
+  {
+    id: "2",
+    title: "Write some code",
+  },
+  {
+    id: "3",
+    title: "Take the test",
+  },
+  {
+    id: "4",
+    title: "Excercise",
+  },
+];
+const Item = ({ title }: any) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
+export default function Home() {
+  const [refreshing, setrefreshing] = useState(false);
+  const [data, setdata] = useState(DATA);
+  const onRefresh = () => {
+    setrefreshing(true);
+    setTimeout(() => {
+      setdata((data) => [
+        ...data,
+        {
+          id: "57878",
+          title: "Take a walk in the park",
+        },
+      ]);
+      setrefreshing(false);
+    }, 2000);
+  };
+  const renderItem = ({ item }: any) => <Item title={item.title} />;
   return (
-    <View>
-      <Text>Home</Text>
-      <Button onPress={() => navigation.navigate()} />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+        refreshing={refreshing}
+        onRefresh={onRefresh}
+      />
+    </SafeAreaView>
   );
-};
-
-export default Home;
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  item: {
+    backgroundColor: "#fad586",
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 20,
+  },
+});
